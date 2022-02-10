4bd9721133f4000bf8fa2b57b3f76e89e4d13d66
rodwel001 <99386999+rodwel001@users.noreply.github.com>
2022-02-10 07:12:36 +0000

    Add files via upload

---
+++ version_control.R
@@ -0,0 +1,16 @@
+#Valuation Model Version Control
+
+
+vars <- c("BEL","expenses","annuity")
+
+for (v in vars){
+  print(v)
+}
+
+#test
+
+#adding a new branch
+
+#commit to master
+
+#push to github
